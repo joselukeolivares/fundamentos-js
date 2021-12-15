ab2e24855d5a49779a276533056800a353526830
jose luis olivares <joselukeolivares@gmail.com>
2021-12-15 11:35:06 -0700

    se creo archivo de ejercios array

--- array.js
+++ array.js
@@ -10,4 +10,5 @@ frutasArray.push("Melon") //Metodo para agregar elementos en array al final
 frutasArray.pop() //Metodo para eliminar ultimo elemento del array
 frutasArray.shift() //Metodo para eliminar primer elemento del array
 frutasArray.unshift()//Metodo para agregar elementos en array al inicio
+frutasArray.indexOf("Cereza") //Metodo para obtener el indice de un elemento en el array
 
